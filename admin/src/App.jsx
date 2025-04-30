6318f79eb62e5f8bdf90b61d61b32bebef99a043
vaibhavOllato <vaibhav.ollato@gmail.com>
2025-04-30 17:56:37 +0530

    admin setup

---
+++ admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex font-bold items-center justify-center bg-gray-100 text-2xl text-cyan-700">
+      Admin Panel Ready with Tailwind 🚀
+    </div>
+  );
+};
+
+export default App;
